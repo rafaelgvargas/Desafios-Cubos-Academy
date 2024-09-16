17a0b3ab350fedbc676d8637a4df1c30b5e6a548
Rafael Gonçalves Vargas <124538681+rafaelgvargas@users.noreply.github.com>
2024-09-16 15:56:27 -0300

    Desafio-02-figurasGeometricas.ts Resolvido

--- desafio-logica-m01-dds-t19-main/02-figurasGeometricas.ts
+++ desafio-logica-m01-dds-t19-main/02-figurasGeometricas.ts
@@ -1,3 +1,14 @@
 function solucao(quantidadeLados: number): string {
-    // seu código aqui
+    if (quantidadeLados === 3) {
+        return 'TRIANGULO'
+    } else if (quantidadeLados === 4){
+        return 'QUADRILATERO'
+    } else if (quantidadeLados === 5) {
+        return 'PENTAGONO'
+    } else if (quantidadeLados === 6) {
+        return 'HEXAGONO'
+    } else {
+        return 'HEPTAGONO'
+    }
+        
 }
