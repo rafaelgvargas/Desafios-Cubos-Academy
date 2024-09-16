c007124b1a904fd44daec8687869934aa1f0ed03
Rafael Gonçalves Vargas <124538681+rafaelgvargas@users.noreply.github.com>
2024-09-16 15:59:41 -0300

    Desafio-05-descontoGrandesCompras.ts Resolvido

--- desafio-logica-m01-dds-t19-main/05-descontoGrandesCompras.ts
+++ desafio-logica-m01-dds-t19-main/05-descontoGrandesCompras.ts
@@ -1,3 +1,11 @@
 function solucao(precosCarrinho: number[], minimoParaDesconto: number, desconto: number): number {
-  // seu código aqui
+    let valorDaCompra = 0
+    for (let i = 0; i < precosCarrinho.length; i++){
+        valorDaCompra += precosCarrinho[i]
+    }    
+    if (valorDaCompra >= minimoParaDesconto) {
+        return valorDaCompra - (valorDaCompra * desconto)
+    }else {
+        return valorDaCompra
+    }
 }
